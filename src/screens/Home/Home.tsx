826d65b468db20f1e68e08b31c7e9f72553b623c
TowhidAust <towhidaustcse33@gmail.com>
2024-08-14 19:05:35 +0600

    Added some shop cards for home page

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import {globalStyles} from '../../styles';
+import Card from '../../components/Card/Card';
 
-type PropTypes = {
-  navigation: any;
-};
-
-export default function Home(props: PropTypes) {
-  const {navigation} = props;
+export default function Home() {
   return (
-    <View style={globalStyles.container}>
-      <Text>I am home. lets go to somewhere else</Text>
-      <Picker />
-      <Button title="Submit" onPress={() => navigation.navigate('Login')} />
-    </View>
+    <ScrollView>
+      <View style={globalStyles.container}>
+        <Text style={globalStyles.title}>Workshop Nearby</Text>
+        <Card
+          title="MotoX"
+          description="A stunning view of the sunset over the mountains."
+          imageUrl="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg"
+          // onPress={handleCardPress}
+        />
+        <Card
+          title="Techno Service Station"
+          description="A stunning view of the sunset over the mountains."
+          imageUrl="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1723593600&semt=ais_hybrid"
+          // onPress={handleCardPress}
+        />
+        <Card
+          title="Motosurgery"
+          description="A stunning view of the sunset over the mountains."
+          imageUrl="https://marketplace.canva.com/EAFauoQSZtY/1/0/1600w/canva-brown-mascot-lion-free-logo-qJptouniZ0A.jpg"
+          // onPress={handleCardPress}
+        />
+        <Card
+          title="BD Moto Shop"
+          description="A stunning view of the sunset over the mountains."
+          imageUrl="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1723593600&semt=ais_hybrid"
+          // onPress={handleCardPress}
+        />
+      </View>
+    </ScrollView>
   );
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,6 +2,11 @@ import {StyleSheet} from 'react-native';
 
 export const globalStyles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 5,
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
   },
 });
